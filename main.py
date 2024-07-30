93585fadd7b6fbe63376b4f5f47b695a4d2a4228
Fariha Hasan <130080229+Fariha223@users.noreply.github.com>
2024-07-30 12:31:04 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random
-from replit import clear
 from art import logo
 
 
+def clear_console():
+    print("\n" * 100)
+
 def deal_card():
-  """Returns a random card from the deck."""
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
   card = random.choice(cards)
   return card
